63d0224b2b198f4f12e7c074c63fc86a37ebe2e6
EC2 Default User <ec2-user@ip-172-31-38-181.us-east-2.compute.internal>
2018-09-30 18:59:38 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,11 +12,59 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings = Movie.all_ratings
-    @selected_ratings = params[:ratings]
-    @movies = Movie.where(:rating => @selected_ratings.keys)
+    page_change = false
+    
+    if params[:ratings] != nil
+      session[:ratings] = params[:ratings]
+    elsif params[:ratings] == nil
+      params[:ratings] = session[:ratings]
+    end
+    
+    #If the params not nil, set the session to those params
+    if params[:sort]
+      @sort_choice = params[:sort]
+      session[:sort] = params[:sort]
+    #Session has previously been set, so make @sort_choice those values
+    elsif session[:sort]
+      @sort_choice = session[:sort]
+      page_change = true
+    #The titles and release date were not selected
+    else
+      @sort_choice = nil
+    end
+    
+    #If the button for refreshing page is hit without any ratings assigned
+    if params[:commit] == "Refresh" && params[:ratings].nil?
+      @selected_ratings = nil
+      session[:ratings] = nil
+    #params has ratings to be sorted by
+    elsif params[:ratings]
+      @selected_ratings = params[:ratings]
+    #params has no ratings so resort back to session ratings
+    elsif session[:ratings]
+      @selected_ratings = session[:ratings]
+      page_change = true
+    else
+      @selected_ratings = nil
+    end
+    
+    if page_change
+      flash.keep
+      redirect_to movies_path :sort => @sort_choice, :ratings => @selected_ratings
+    end
+    
+    if @sort_choice == 'title' and @selected_ratings != nil
+      @movies = Movie.where(:rating => @selected_ratings.keys).order(:title)
+      @title_head = 'hilite'
+    elsif @sort_choice == 'rating' and @selected_ratings != nil
+      @movies = Movie.where(:rating => @selected_ratings.keys).order(:release_date)
+      @release_head = 'hilite'
+    end
     
     
   end
+
+
 
   def new
     # default: render 'new' template
